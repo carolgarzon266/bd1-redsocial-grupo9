e8455401a5e69bb9ee3c97b6425a71606dda961c
carolgarzon266 <carol.garzon266@pascualbravo.edu.co>
2026-04-19 22:17:10 -0500

    Delete Tarea 4/Scripts/20261-bd1-g100-tia-04-E-9-scripts-03-sqlserver-crea.sql

--- Tarea 4/Scripts/20261-bd1-g100-tia-04-E-9-scripts-03-sqlserver-crea.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE publicacion (
-    id_publicacion INT IDENTITY(1,1) PRIMARY KEY,
-    id_usuario INT,
-    contenido NVARCHAR(MAX),
-    metadatos_iot NVARCHAR(MAX), -- Almacena JSON con restricción ISJSON
-    CONSTRAINT FK_Usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-);
-
